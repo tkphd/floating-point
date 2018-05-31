d6cd26b94a09e311c80b9e8eb91021a8d277d234
Trevor Keller <trevor.keller@nist.gov>
2018-05-31 15:32:55 -0400

    public domain and docstrings

--- summation.cpp
+++ summation.cpp
@@ -1,18 +1,53 @@
-/**
-   \file shuffle-sum.cpp
-   \brief map histogram of values which, ideally, would all be 10000
+/*******************************************************************************
+This software was developed by employees of the National Institute of Standards
+and Technology (NIST), an agency of the Federal Government. Pursuant to Title 17
+United States Code Section 105, works of NIST employees are not subject to
+copyright protection in the United States and are considered to be in the public
+domain. Permission to freely use, copy, modify, and distribute this software and
+its documentation without fee is hereby granted, provided that this notice and
+disclaimer of warranty appears in all copies.
 
-   In principal, \$ 10\left\frac{1}{1000}\right)
-                   + 9\left\frac{1}{ 100}\right)
-                   + 9\left\frac{1}{  10}\right)
-                   + 9\left(           1\right)
-                   + 9\left(          10\right)
-                   + 9\left(         100\right)
-                   + 9\left(        1000\right)
-                   = 10000 \$.
-   This program tests the reality by generating these numbers,
-   shuffling them at random, then taking their sum: the order
-   of addition seems to matter.
+THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER
+EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY
+THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM INFRINGEMENT,
+AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY
+WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE. IN NO EVENT SHALL NIST BE LIABLE
+FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL OR
+CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN ANY WAY CONNECTED
+WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY, CONTRACT, TORT, OR
+OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY PERSONS OR PROPERTY OR
+OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT OF THE
+RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
+
+Distributions of NIST software should also include copyright and licensing
+statements of any third-party software that are legally bundled with the code
+in compliance with the conditions of those licenses.
+*******************************************************************************/
+
+/**
+   \file summation.cpp
+   \brief map histogram of values
+
+   In principal, \$ 10 \times 10^{-3}
+                  +  9 \times 10^{-2}
+                  +  9 \times 10^{-1}
+                  +  9 \times 10^{0}
+                  +  9 \times 10^{1}
+                  +  9 \times 10^{2}
+                  +  9 \times 10^{3}
+                  = 10000.\$
+   Similarly, \$ 8 \times 2^{-4}
+               + 8 \times 2^{-3}
+               + 8 \times 2^{-2}
+               + 9 \times 2^{-1}
+               + 8 \times 2^{0}
+               + 8 \times 2^{1}
+               + 8 \times 2^{2}
+               + 8 \times 2^{3}
+               = 128.\$
+   This program demonstrates that the decimal power series is approximate, not
+   associative, while the binary power series is exact.
  */
 
 #include <algorithm>
@@ -41,6 +76,7 @@ int main()
         x.push_back(1000.);
     }
     x.push_back(0.001); // there's an extra one of these guys
+
     #else
     for (unsigned int i = 0; i < 8; i++) {
         x.push_back(0.0625);
@@ -53,6 +89,7 @@ int main()
         x.push_back(8.0000);
     }
     x.push_back(0.5000); // there's an extra one of these guys
+
     #endif
 
     for (unsigned int i = 0; i < N; i++) {
